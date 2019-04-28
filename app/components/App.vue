e1703150fed438450d494be01f66e8895f1f5e30
Piccard <piccard@web.de>
2019-04-28 12:24:15 +0200

    Actionbar & splash

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,10 +1,19 @@
 <template>
-    <Page class="page">
-        <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/>
-        <TabView android:tabBackgroundColor="#53ba82"
-                 android:tabTextColor="#c4ffdf"
-                 android:selectedTabTextColor="#ffffff"
-                 androidSelectedTabHighlightColor="#ffffff">
+    <Page class="page" @loaded="pageLoaded">
+        <!-- <ActionBar  class="action-bar" title="Zuckerwasser Rechner" android:flat="true" android.icon="res://icon"  android.iconVisibility="always" /> -->
+       
+        <ActionBar>
+            <StackLayout orientation="horizontal">
+                <Image src="res://icon" width="40" height="40" verticalAlignment="center" class="mr-4"/>
+                <Label text="Zuckerwasser-Rechner" fontSize="24" verticalAlignment="center" />
+            </StackLayout>
+        </ActionBar>
+
+        <TabView :selectedIndex="0"
+                 :android:tabBackgroundColor="primaryColor"
+                 :android:tabTextColor="secondaryColor"
+                 :android:selectedTabTextColor="secondaryColor"
+                 :androidSelectedTabHighlightColor="secondaryColor">
             <TabViewItem title="Tab 1">
                 <GridLayout columns="*" rows="*">
                     <Label class="message" :text="msg" col="0" row="0"/>
@@ -25,25 +34,52 @@
 </template>
 
 <script >
+  import * as color from "tns-core-modules/color";
+  import * as application from "tns-core-modules/application";
+  import * as platform from "tns-core-modules/platform";
+
+  const primaryColor = "#ffc107";
+  const secondaryColor = "#ffffff";
+
   export default {
     data() {
       return {
         msg: 'Hello World 21!'
       }
+    },
+    computed: {
+        primaryColor() {
+            return primaryColor;
+        },
+        secondaryColor() {
+            return secondaryColor;
+        }
+    },
+    methods: {
+        pageLoaded() {
+            if (application.android && platform.device.sdkVersion >= "21") {
+                const window = application.android.foregroundActivity.getWindow();
+                window.setStatusBarColor(new color.Color(this.primaryColor).android);
+            }
+        }
     }
   }
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
+     ActionBar {
+        background-color: #ffc107;
         color: #ffffff;
-    }
+    } 
 
     .message {
         vertical-align: center;
         text-align: center;
         font-size: 20;
         color: #333333;
+    } 
+
+    .mr-4 {
+        margin-right: 40px;
     }
 </style>
